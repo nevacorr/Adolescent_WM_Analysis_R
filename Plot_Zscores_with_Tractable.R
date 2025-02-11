0e49b892981492a05ce7fc35e4c6e03cb8ae0720
nevao <nevao@uw.edu>
2025-02-11 15:55:46 -0800

    plot tract z scores for all tracts fa, md, mpf

--- Plot_Zscores_with_Tractable.R
+++ Plot_Zscores_with_Tractable.R
@@ -7,7 +7,7 @@ rm(list = ls())
 
 data_dir = "/Users/nevao/Documents/Adol_WM_Data/Z_scores_time_2_1_split"
 metric = "md"
-data_filename = paste0("Z_time2_", metric, "_1_splits_6regs.csv")
+data_filename = paste0("Z_time2_", metric, "_1_split_all_tracts.csv")
 
 source("load_multinode_tract_data.R")
 
@@ -15,13 +15,30 @@ df_z = load_data(data_dir, data_filename, metric)
 
 # Visualize the data
 
-tractable::plot_tract_profiles(
+plot_tract_profiles(
   df = df_z, 
-  metrics = "z", 
-  # bundles = c("Left.IFOF", "Right.IFOF"),
+  y = "z", 
+  # tracts = c("Left.IFOF", "Right.IFOF"),
   group_col = "sex",
   n_groups = 2,
-  figsize   = c(8, 8),
-  pal_name = "Spectral"
+  save_figure = FALSE,
+  ribbon_alpha = 0.20,
+  group_pal = "Set1"
 )
+
+# Convert sex to factors
+df_z <- df_z %>%
+  mutate(
+    sex = factor(sex)
+  )
+
+gam_fit = tractable_single_tract(
+  df             = df_z,
+  tract          = "Right.ILF",
+  target         = "z",
+  regressors      = c("sex")
+)
+
+summary(gam_fit)
+
 
