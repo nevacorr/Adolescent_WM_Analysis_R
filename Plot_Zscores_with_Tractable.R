25c25042c670fdb373d84684ce87cfea28530380
nevao <nevao@uw.edu>
2025-02-11 16:37:24 -0800

    Remove NAs from data Loop through all tracts Output GAM statistics into a table

--- Plot_Zscores_with_Tractable.R
+++ Plot_Zscores_with_Tractable.R
@@ -6,7 +6,7 @@ library(dplyr)
 rm(list = ls())
 
 data_dir = "/Users/nevao/Documents/Adol_WM_Data/Z_scores_time_2_1_split"
-metric = "fa"
+metric = "md"
 data_filename = paste0("Z_time2_", metric, "_1_split_all_tracts.csv")
 
 source("load_multinode_tract_data.R")
@@ -33,13 +33,48 @@ df_z <- df_z %>%
     sex = factor(sex)
   )
 
-gam_fit = tractable_single_tract(
-  df             = df_z,
-  tract          = "Right.ILF",
-  target         = "z",
-  regressors      = c("sex")
-)
+# Get unique tract names
+unique_tracts <- unique(df_z$tractID)
 
-summary(gam_fit)
+df_z <- df_z %>% filter(!is.na(z))
+
+# Function to extract full statistics from GAM model
+extract_gam_stats <- function(model, tract) {
+  model_summary <- summary(model)  # Get model summary
+  
+  # Extract R-squared, Deviance explained, EDF, F-statistic, p-values
+  intercept_p_value <- model_summary$s.table[1, "p-value"]  # Intercept p-value
+  sex_p_value <- model_summary$s.table[2, "p-value"]  # p-value for sexM term
+  rsq <- model_summary$r.sq  # R-squared
+  deviance_explained <- model_summary$dev.expl  # Deviance explained
+  edf <- model_summary$edf[1]  # Effective degrees of freedom for the smooth term
+  f_statistic <- model_summary$s.table[1, "F"]  # F-statistic for the intercept
+  
+  tibble(
+    tractID = tract,
+    rsq = rsq,
+    deviance_explained = deviance_explained,
+    edf = edf,
+    intercept_p_value = intercept_p_value,
+    sex_p_value = sex_p_value,
+    f_statistic = f_statistic
+  )
+}
+
+# Run tractable_single_tract() for each tract and collect stats
+tract_summary <- map_df(unique_tracts, ~ {
+  model <- tractable_single_tract(
+    df        = df_z,
+    tract     = .x,
+    target    = "z",
+    regressors = c("sex")
+  )
+  extract_gam_stats(model, .x)  # Extract all statistics
+})
+
+# Print the summary
+print(tract_summary)
+  
+
 
 
