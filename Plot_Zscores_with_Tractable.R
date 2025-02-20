ec8016433767da7c22a13a702bc00c6129a71011
nevao <nevao@uw.edu>
2025-02-19 17:00:56 -0800

    fixed loop outputing model stats

--- Plot_Zscores_with_Tractable.R
+++ Plot_Zscores_with_Tractable.R
@@ -6,7 +6,7 @@ library(dplyr)
 rm(list = ls())
 
 data_dir = "/Users/nevao/Documents/Adol_WM_Data/Z_scores_time_2_1_split"
-metric = "mpf"
+metric = "md"
 data_filename = paste0("Z_time2_", metric, "_1_split_all_tracts.csv")
 
 source("load_multinode_tract_data.R")
@@ -38,43 +38,44 @@ unique_tracts <- unique(df_z$tractID)
 
 df_z <- df_z %>% filter(!is.na(z))
 
-# Function to extract full statistics from GAM model
-extract_gam_stats <- function(model, tract) {
-  model_summary <- summary(model)  # Get model summary
-  
-  # Extract R-squared, Deviance explained, EDF, F-statistic, p-values
-  intercept_p_value <- model_summary$s.table[1, "p-value"]  # Intercept p-value
-  sex_p_value <- model_summary$s.table[2, "p-value"]  # p-value for sexM term
-  rsq <- model_summary$r.sq  # R-squared
-  deviance_explained <- model_summary$dev.expl  # Deviance explained
-  edf <- model_summary$edf[1]  # Effective degrees of freedom for the smooth term
-  f_statistic <- model_summary$s.table[1, "F"]  # F-statistic for the intercept
-  
-  tibble(
-    tractID = tract,
-    rsq = rsq,
-    deviance_explained = deviance_explained,
-    edf = edf,
-    intercept_p_value = intercept_p_value,
-    sex_p_value = sex_p_value,
-    f_statistic = f_statistic
-  )
-}
+results_df <- data.frame(
+  metric = character(),
+  tract = character(),
+  intercept_p = numeric(),
+  sex_p = numeric(),
+  R_sq = numeric(),
+  deviance_explained = numeric(),
+  stringsAsFactors = FALSE
+)
 
-# Run tractable_single_tract() for each tract and collect stats
-tract_summary <- map_df(unique_tracts, ~ {
-  model <- tractable_single_tract(
-    df        = df_z,
-    tract     = .x,
-    target    = "z",
+# # Run tractable_single_tract() for each tract and collect stats
+
+for (tract in unique_tracts) {
+  # Fit the model
+  model <-  tractable_single_tract(
+    df = df_z,
+    tract = tract,
+    target = 'z',
     regressors = c("sex")
   )
-  extract_gam_stats(model, .x)  # Extract all statistics
-})
 
-# Print the summary
-print(tract_summary)
+  model_summary = summary(model)
+
+  sex_p_value = model_summary$p.table["sexM", "Pr(>|t|)"]
+  intercept_p_value = model_summary$p.table["(Intercept)", "Pr(>|t|)"]
+  R_sq = model_summary$r.sq
+  dev_exp = model_summary$dev.expl
   
+  # Put results in dataframe
+  results_df <- rbind(results_df, data.frame(
+    metric = metric,
+    tract = tract,
+    intercept_p = sprintf("%.3f", intercept_p_value),
+    sex_p = sprintf("%.3f",sex_p_value),
+    R_sq = sprintf("%.3f",R_sq),
+    deviance_explained = sprintf("%.3f",dev_exp)
+  ))
+  
+}
 
-
-
+print(results_df)
