0fbee6eba945dfc0c358c4d178b52bb95592e4fb
nevao <nevao@uw.edu>
2025-02-19 16:59:39 -0800

    Fixed loop outputing model results

--- Plot_Zscores_with_Tractable.R
+++ Plot_Zscores_with_Tractable.R
@@ -6,7 +6,7 @@ library(dplyr)
 rm(list = ls())
 
 data_dir = "/Users/nevao/Documents/Adol_WM_Data/Z_scores_time_2_1_split"
-metric = "md"
+metric = "mpf"
 data_filename = paste0("Z_time2_", metric, "_1_split_all_tracts.csv")
 
 source("load_multinode_tract_data.R")
