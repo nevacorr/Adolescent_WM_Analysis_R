3f7b91987bae41ccaec044bb00bed868ae67ca1d
nevao <nevao@uw.edu>
2025-02-20 13:27:40 -0800

    Move make model to separate file

--- Plot_Zscores_with_Tractable.R
+++ Plot_Zscores_with_Tractable.R
@@ -38,45 +38,9 @@ unique_tracts <- unique(df_z$tractID)
 
 df_z <- df_z %>% filter(!is.na(z))
 
-results_df <- data.frame(
-  metric = character(),
-  tract = character(),
-  intercept_p = numeric(),
-  sex_p = numeric(),
-  R_sq = numeric(),
-  deviance_explained = numeric(),
-  stringsAsFactors = FALSE
-)
+
 
 # # Run tractable_single_tract() for each tract and collect stats
 
-for (tract in unique_tracts) {
-  # Fit the model
-  model <-  tractable_single_tract(
-    df = df_z,
-    tract = tract,
-    target = 'z',
-    regressors = c("sex"),
-    node_group = "sex"
-  )
-
-  model_summary = summary(model)
-
-  sex_p_value = model_summary$p.table["sexM", "Pr(>|t|)"]
-  intercept_p_value = model_summary$p.table["(Intercept)", "Pr(>|t|)"]
-  R_sq = model_summary$r.sq
-  dev_exp = model_summary$dev.expl
-  
-  # Put results in dataframe
-  results_df <- rbind(results_df, data.frame(
-    metric = metric,
-    tract = tract,
-    intercept_p = sprintf("%.3f", intercept_p_value),
-    sex_p = sprintf("%.3f",sex_p_value),
-    R_sq = sprintf("%.3f",R_sq),
-    deviance_explained = sprintf("%.3f",dev_exp)
-  ))
-  
-}
 
 print(results_df)
