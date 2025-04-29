9b717fc6813b40018e3e1c12850866b407a41062
nevao <nevao@uw.edu>
2025-04-29 12:51:56 -0700

    add summary of significant tracts and output node p values

--- Plot_Zscores_with_Tractable_and_itsadug.R
+++ Plot_Zscores_with_Tractable_and_itsadug.R
@@ -17,6 +17,11 @@ metric <-  "md"
 splits <-  100
 data_filename = paste0("Z_time2_", metric, "_", splits, "_splits.csv")
 
+# define path of image output
+output_image_path <- "/Users/nevao/R_Projects/AdolWMAnalysis/tract profile plots"
+# define path of node stats output
+output_stats_path <- "/Users/nevao/R_Projects/AdolWMAnalysis/tract stats files"
+
 # read data file
 z_orig <- read.csv(file.path(data_dir, data_filename))
 
@@ -27,27 +32,54 @@ if ("split" %in% colnames(z_orig)) {
   z_orig <- average_across_splits(z_orig)
 }
 
+# convert data to long format and add sex and age columns
 df_z = reformat_data(z_orig)
 
-plot_tracts(df_z, 1)
+# plot all tracts by sex
+plot_tracts(df_z, 1, output_image_path)
 
-# Convert sex to factor
+# convert sex to factor
 df_z <- df_z %>%
   mutate(
     sex = factor(sex)
   )
 
+# convert subjectID to factor
 df_z$subjectID <- factor(df_z$subjectID)
 
 # Get unique tract names
 unique_tracts <- unique(df_z$tractID)
 
+# remove rows without zscores
 df_z <- df_z %>% filter(!is.na(z))
 
 # # Run tractable_single_tract() for each tract and collect stats
 output <-  run_tractable_single_tract_and_itsadug(df_z, unique_tracts, metric)
 print(metric)
 print(output$results_df)
-print("\n")
-print(output$node_pvalues)
+
+significant_tracts_sex_difference <- output$results_df %>%
+  filter(sex_p_fdr <= 0.05) %>% 
+  pull(tract)
+
+significant_tracts_male <- output$results_df %>% 
+  filter(male_p_fdr <= 0.05 ) %>% 
+  pull(tract)
+
+significant_tracts_female <- output$results_df %>% 
+  filter(female_p_fdr <= 0.05 ) %>% 
+  pull(tract)
+
+print(paste("Tracts with significant post-covid sex differences in", metric, ":"))
+cat(significant_tracts_sex_difference, sep = "\n")
+
+print(paste("Tracts with significantly different",metric,"values post-covid for males:"))
+cat(significant_tracts_male, sep = "\n")
+
+print(paste("Tracts with significantly different",metric,"values post-covid for females:"))
+cat(significant_tracts_female, sep = "\n")
+
+node_vals = output$node_pvalues
+output_node_file = file.path(output_stats_path, paste0(metric, "_node_stats_gam.csv"))
+write.csv(node_vals, output_node_file, row.names = FALSE)
 
