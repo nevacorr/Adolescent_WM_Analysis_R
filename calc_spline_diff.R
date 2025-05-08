7c0f5019d577f1cf8df506af03d7d645fef980cc
nevao <nevao@uw.edu>
2025-05-08 15:41:14 -0700

    initial commit. Code from N. Muncy 2022 Supplementary Materials.

---
+++ calc_spline_diff.R
@@ -0,0 +1,66 @@
+
+# This code is from Supplementary Materials for NM Muncy, A Kimbler, 
+# AM Hedges-Muncy, DL McMakin, AT Mattfeld. General additive models address 
+# statistical issues in diffusion MRI: An example with clinically anxious adolescents. 
+# Neuroimage Clin 2022:33:102937. doi: 10.1016/j.nicl.2022.102937. Epub 2022 Jan 5.
+
+calc_spline_diff <- function(gam_model, 
+                             tract, 
+                             comp_list) {
+  # Investigate whether differences exist between spline fit FAs
+  #
+  # This function does a number of things:
+  #   1) Makes plots and tables
+  #   2) Make dataframes of difference estimates
+  #   3) Determine nodes that differ in difference estimation 
+  #       for average. Repeats what is done in tables.
+  #       Also gets node of maximum difference.
+  #
+  # Arguments:
+  #   gam_model = GAM object, produced by gam/bam
+  #   tract = AFQ tract name
+  #   comp_list = list of 2 factors for pairwise comparison
+  #
+  # Returns:
+  #   diff_list, node_max = indexed list of nodes which differed
+  #     [[1]] = nodes which differed, [[2]] = node of maximum difference
+  
+  browser()
+  
+  # make plots and tables
+  plot_spline_diff(gam_model, tract, comp_list[1], comp_list[2])
+  
+  browser()
+  
+  # get plot_diff data frames
+  df_est_diff <- make_spline_diff_df(gam_model, comp_list[1], comp_list[2])
+  
+  browser()
+  
+  # determine where nodes differ
+  node_list <- unique(df_est_diff$nodeID)
+  diff_list <- vector()
+  
+  browser()
+  
+  for (node in node_list) {
+    ind_node <- which(df_est_diff$nodeID == node)
+    h_est <- abs(df_est_diff[ind_node[1], ]$est)
+    h_ci <- df_est_diff[ind_node[1], ]$CI
+    if ((h_est - h_ci) > 0) {
+      diff_list <- c(diff_list, node)
+    }
+  }
+  
+  browser()
+  
+  # find node of max difference
+  h_df <- subset(df_est_diff, nodeID %in% diff_list)
+  ind_max <- which(abs(h_df$est) == max(abs(h_df$est)))
+  node_max <- h_df[ind_max, ]$nodeID
+  
+  browser()
+  
+  return(list(diff_list, node_max))
+}
+
---
+++ plot_spline_diff.R
@@ -0,0 +1,92 @@
+# This code is from Supplementary Materials for NM Muncy, A Kimbler, 
+# AM Hedges-Muncy, DL McMakin, AT Mattfeld. General additive models address 
+# statistical issues in diffusion MRI: An example with clinically anxious adolescents. 
+# Neuroimage Clin 2022:33:102937. doi: 10.1016/j.nicl.2022.102937. Epub 2022 Jan 5.
+
+plot_spline_diff <- function(gam_model, 
+                             tract, 
+                             factor_a, 
+                             factor_b) {
+  # Draw a spline-difference plot for 2 splines
+  #
+  # This will make plots and write tables of sig
+  # node differences for GAM splines bx 2 factors (groups)
+  #
+  # Arguments:
+  #   gam_model = GAM object, produced by gam/bam
+  #   tract = AFQ tract name
+  #   factor_a = group factor (0-2), string
+  #   factor_b = group factor (0-2), string
+  #
+  # Writes:
+  #   gam_plot_dir/Plot_Diff_*_pair.png
+  #   table_dir/Table_Diff_*.txt
+  
+  # setup for plotting
+  group_a <- switch_plot_values(factor_a)[[2]][1]
+  group_b <- switch_plot_values(factor_b)[[2]][1]
+  
+  # determine sig nodes
+  p_summary <- capture.output(plot_diff(gam_model,
+                                        view = "nodeID",
+                                        comp = list(group = c(factor_a, factor_b)),
+                                        rm.ranef = T
+  ))
+  sig_regions <- p_summary[10:length(p_summary)]
+  sig_regions <- gsub("\\t", "", sig_regions)
+  
+  # make list of start and end nodes, for shading
+  sig_list <- as.list(strsplit(sig_regions, " - "))
+  start_list <- as.numeric(sapply(sig_list, "[[", 1))
+  end_list <- as.numeric(sapply(sig_list, "[[", 2))
+  
+  # determine bottom of plot
+  p_est <- plot_diff(gam_model,
+                     view = "nodeID",
+                     comp = list(group = c(factor_a, factor_b)),
+                     rm.ranef = T,
+                     plot = F
+  )
+  h_min <- min(p_est$est)
+  h_ci <- p_est[which(p_est$est == h_min), ]$CI
+  min_val <- h_min - h_ci
+  
+  # set output
+  png(
+    filename = paste0(
+      gam_plot_dir, "Plot_Diff_", tract, "_G3_pair.png"
+    ),
+    width = 600, height = 600
+  )
+  
+  # draw plot
+  par(mar = c(5, 5, 4, 2), family = "Times New Roman")
+  plot_diff(gam_model,
+            view = "nodeID",
+            comp = list(group = c(factor_a, factor_b)),
+            rm.ranef = T,
+            main = paste0(
+              "Difference Scores, ", group_a, "-", group_b
+            ),
+            ylab = "Est. FA difference",
+            xlab = "Tract Node",
+            cex.lab = 2,
+            cex.axis = 2,
+            cex.main = 2,
+            cex.sub = 1.5,
+            col.diff = "red"
+  )
+  
+  # shade significant regions
+  for (h_ind in 1:length(start_list)) {
+    polygon(
+      x = c(rep(start_list[h_ind],2), rep(end_list[h_ind], 2)), 
+      y = c(0, min_val, min_val, 0), 
+      col = rgb(1, 0, 0, 0.2), 
+      border = NA
+    )
+  }
+  
+  par(mar = c(5, 4, 4, 2))
+  dev.off()
+}
