884f1332e859ca52b1ac09fb9befa961ee89dddc
nevao <nevao@uw.edu>
2025-05-21 15:59:34 -0700

    remove standard error shading on plot

---
+++ plot_behav_vs_measure.R
@@ -0,0 +1,19 @@
+plot_behav_vs_measure <- function(data, tract_name, brain_measure_name, behav_measure_name,
+                                  metric) {
+  # Filter for the specified tract
+  tract_data <- data %>%
+    filter(tractID == tract_name)
+  
+  # make a plot
+  p <- ggplot(tract_data, aes_string(x = brain_measure_name, y = behav_measure_name)) +
+    geom_point(alpha = 0.6) +
+    geom_smooth(method = "lm", se = TRUE, color = "blue") +
+    labs(
+      title = paste("Post-COVID Z", metric, "vs. Z", behav_measure_name, "in", tract_name),
+      x = metric,
+      y = behav_measure_name
+    ) +
+    theme_minimal()
+  
+  return(p)
+}
