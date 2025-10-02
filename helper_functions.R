db57b03ddd518ee9ea81139170dae08b9ae7784c
nevao <nevao@uw.edu>
2025-10-02 13:16:47 -0700

    change spacing in x axis tracxt profile label

--- helper_functions.R
+++ helper_functions.R
@@ -2,7 +2,7 @@
 get_x_axis_string <- function(tract_name) {
   case_when(
     grepl("Left", tract_name) & grepl("Arcuate|Thalamic|ILF|IFOF|SLF|Uncinate", 
-                                      tract_name) ~ "Anterior                        Posterior",
+                                      tract_name) ~ "Anterior                    Posterior",
     grepl("Right", tract_name) & grepl("Arcuate|Thalamic|ILF|IFOF|SLF|Uncinate"
                                        , tract_name) ~ "Posterior                     Anterior",
     grepl("Corticospinal", tract_name) ~ "Inferior                       Superior",
