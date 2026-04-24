b8fc921684e33b337d79b6298791eea5dce91c78
nevao <nevao@uw.edu>
2026-04-24 15:09:20 -0700

    add node-wise calculations back in

--- run_tractable_single_tract_model.R
+++ run_tractable_single_tract_model.R
@@ -5,19 +5,14 @@ run_tractable_single_tract_model <- function(df_z_both_sexes, df_z,
                                              output_image_path,
                                              sex_str) {
   
-  # -------------------------------
-  # OUTPUT TABLE
-  # -------------------------------
+  # Make an empty dataframe to store tract-level statistics
   results_df <- data.frame(
     metric = character(),
     tract = character(),
-    
     intercept_p = numeric(),
-    
     effect_size = numeric(),
     ci_low = numeric(),
     ci_high = numeric(),
-    
     stringsAsFactors = FALSE
   )
   
@@ -28,6 +23,7 @@ run_tractable_single_tract_model <- function(df_z_both_sexes, df_z,
     results_df$sex_ci_high <- numeric()
   }
   
+  # Make an empty dataframe to store t-test p-values for each node for each tract
   node_muncy_pvalues_all <- data.frame(
     Node = integer(), 
     P_value = numeric(), 
@@ -101,7 +97,27 @@ run_tractable_single_tract_model <- function(df_z_both_sexes, df_z,
       beta <- NA
       sex_ci_low <- NA
       sex_ci_high <- NA
+      
+      # calculate node values
+      node_output <- run_single_tract(df_z_both_sexes, df_z, tract, metric, sex_str)
+      
+      # Compute mean z for each node for this tract
+      df_z_mean <- df_z %>%
+        filter(tractID == tract) %>%      # keep only this tract
+        group_by(nodeID) %>%              # group by node
+        summarise(Z_mean = mean(z, na.rm = TRUE)) %>%
+        ungroup()
+      
+      # Combine with p-values
+      node_muncy_pvalues <- df_z_mean %>%
+        left_join(node_output %>% select(nodeID, pvalue), by = "nodeID") %>%
+        rename(Node = nodeID,
+               P_value = pvalue) %>%
+        mutate(tract = tract)            # add tract column
+      
+      node_muncy_pvalues_all <- rbind(node_muncy_pvalues_all, node_muncy_pvalues )
     }
+      
     
     # -------------------------------
     # COMMON METRICS
@@ -112,7 +128,6 @@ run_tractable_single_tract_model <- function(df_z_both_sexes, df_z,
       metric = metric,
       tract = tract,
       intercept_p = sprintf("%.3f", intercept_p_value),
-      
       effect_size = effect_size,
       ci_low = ci_low,
       ci_high = ci_high
